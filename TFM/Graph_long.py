456e505a620b8974237d5888f94640e6f99dc6c0
Andres Muracciole <amuracciole@gmail.com>
2020-05-26 21:49:19 +0200

    Changes

--- TFM/Graph_long.py
+++ TFM/Graph_long.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 
+w = []
 x = []
 y = []
 z = []
@@ -10,11 +11,13 @@ for line in file:
     data = line.split(',')
     x.append(float(data[0]))
     y.append(float(data[1]))
-    z.append(float(data[2]))
+    w.append(float(data[2]))
+    z.append(float(data[3]))
     
 # Creating a numpy array
 X = np.array(x)
 Y = np.array(y)
+W = np.array(w)
 Z = np.array(z)
 
 plt.figure(2)
@@ -22,5 +25,6 @@ plt.title("Bandwidth long-path")
 plt.xlabel("Time (Seg.)")
 plt.ylabel("Bandwidth (Kbps)")
 plt.plot(X,Y, color="blue")
+plt.plot(X,W, color="green", linestyle="--")
 plt.plot(X,Z, color="red", linestyle="--")
 plt.show()
--- TFM/Monitor.py
+++ TFM/Monitor.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import subprocess
 import csv
 import time
 from os import remove
@@ -11,7 +12,10 @@ bw_short_path_1 = 0
 bw_short_path_2 = 0
 bw_short_path_3 = 0
 i=0
-bw_lim = 100000
+secure = 1
+secure_delete = 1
+bw_max = 100000
+bw_lim = 70000
 
 remove("/home/upm/Desktop/TFM/Data_long.txt")
 file = open("/home/upm/Desktop/TFM/Data_long.txt", "a")
@@ -39,11 +43,11 @@ while (True):
     bw_short_path_prom = (bw_short_path_3 + bw_short_path_2 + bw_short_path_1)/3
 
     file = open("/home/upm/Desktop/TFM/Data_long.txt", "a")
-    file.write(str(i) + "," + str("{:.2f}".format(bw_long_path_prom)) + "," + str(bw_lim))
+    file.write(str(i) + "," + str("{:.2f}".format(bw_long_path_prom)) + "," + str(bw_lim) + "," + str(bw_max))
     file.write("\n")
 
     file = open("/home/upm/Desktop/TFM/Data_short.txt", "a")
-    file.write(str(i) + "," + str("{:.2f}".format(bw_short_path_prom)) + "," + str(bw_lim))
+    file.write(str(i) + "," + str("{:.2f}".format(bw_short_path_prom)) + "," + str(bw_lim) + "," + str(bw_max))
     file.write("\n")
 
     i = i + 10
@@ -58,9 +62,17 @@ while (True):
     print ("    % del total libre: " + str("{:.2f}".format(100 - bw_short_path_prom/1000)) + "%")
     print ()
 
-    #if (bw_long_path_prom < 50000):
-    #    print ("menor que el 50 del total")
-    #else:
-    #    print ("mayor que el 50 del total")
+    if (bw_short_path_prom > bw_lim):
+        print ("Menos del 30 libre")
+        if (secure_delete == 1):
+            subprocess.call("/home/upm/Desktop/TFM/Sensible_traffic_long_path.sh 22", shell=True)
+            secure_delete = 0
+            secure = 0
+    else:
+        print ("Mas del 30 libre")
+        if (secure == 0):
+            subprocess.call("/home/upm/Desktop/TFM/Sensible_traffic_long_path_delete.sh 22", shell=True)
+            secure_delete = 1
+            secure = 1
 
     time.sleep(10)
