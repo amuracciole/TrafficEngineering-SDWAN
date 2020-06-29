6ab35598988ccca41853f806170d6b54a4187375
Andres Muracciole <amuracciole@gmail.com>
2020-06-29 17:45:55 +0200

    delete all

--- TFM/Diagrama.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#sudo apt-get install at-spi2-core
-cd /usr/share/vnx/filesystems
-#sudo vnx -f /usr/share/vnx/tfm/Laboratory.xml --show-map
-sudo vnx -f /usr/share/vnx/tfm/Laboratory_full.xml --show-map
--- TFM/ON.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#sudo apt-get install at-spi2-core
-cd /usr/share/vnx/filesystems
-#sudo vnx -f /usr/share/vnx/tfm/Laboratory.xml -v --create
-sudo vnx -f /usr/share/vnx/tfm/Laboratory_full.xml -v --create
-
-
