ed395c0aca0e270419f9e8aaa9ebb1e161e6cd43
Andres Muracciole <amuracciole@gmail.com>
2020-06-29 18:22:08 +0200

    TE_SD-WAN

---
+++ TFM/OFF.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd /usr/share/vnx/filesystems
+sudo vnx -f /usr/share/vnx/tfm/Laboratory.xml -v --destroy
+
+
+
+
---
+++ TFM/ON.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#sudo apt-get install at-spi2-core
+cd /usr/share/vnx/filesystems
+#sudo vnx -f /usr/share/vnx/tfm/Laboratory.xml -v --create
+sudo vnx -f /usr/share/vnx/tfm/Laboratory_full.xml -v --create
+
+
